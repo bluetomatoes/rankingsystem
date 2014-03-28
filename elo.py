2f0aa64eee6fef4361a232df12d03a2094128c2e
Matthew <javanrhino110@gmail.com>
2014-03-28 07:40:19 -0700

    Working on file seeking capability to edit elo

--- elo.py
+++ elo.py
@@ -10,19 +10,23 @@ open('playertest.txt', 'w').close()
 game = []
 winners =[]
 losers = []
-file_searcher = open("playerlist.txt", 'r')
+file_searcher = open("playerlist.txt", 'rw+')
+file_searcher.seek(5,5)
+line = file_searcher.readline()
 playerfile = list(file_searcher)
 playerfile = [line.rstrip('\n') for line in playerfile]
 elofile = [line.split(' ,')[1] for line in playerfile]
 playerfile = [line.split(' ,')[0] for line in playerfile]
 player_scores = {}
-print(elofile)
+print(playerfile)
+
+print("read Line: %s" % (line))
 #for number items in playefile
     #player_scores[player] = playerfile[i]
 for p in range(0,len(playerfile)):
     player = playerfile[p]
-    player_scores[player] = elofile[p]
-#print(player_scores['Matthew'])
+    player_scores[player] = int(elofile[p])
+print(player_scores['Matthew'])
         
 def addplayer(newplayer, elo, file_name):
     file_adder = open(file_name, 'a')
@@ -43,8 +47,17 @@ def score_checker():
     real_score.append(int(score_input.split(',')[0]))
     real_score.append(int(score_input.split(',')[1]))
 def update_elo():
-    winners[0]
-    pass
+    elo1 = player_scores[winners[0]]
+    elo2 = player_scores[winners[1]]
+    elo3 = player_scores[losers[0]]
+    elo4 = player_scores[losers[1]]
+    oldelo1= elo1
+    oldelo2 = elo2
+    opponent_elo = elo3+elo4
+    elo1 = elo1 + (opponent_elo + (opponent_elo - elo1 - elo2)/5)/elo2*0.5
+    elo2 = elo2 + (opponent_elo + (opponent_elo - elo1 - elo2)/5)/elo1*0.5
+    print("Updated elo for "+ str(winners[0]) + ":"+str(oldelo1)+"--->"+str(elo1)+"\n"+"Updated elo for "+str(winners[1])+":"+str(oldelo2)+"---->"+str(elo1))
+    
 while True:
     inpuT = raw_input()
     if inpuT == "n":
@@ -80,25 +93,26 @@ while True:
                         score_checker()
                         
                         if int(gametype_input) == 5:
-                            print("5 received")
-                            if real_score[0] > 5 or real_score[1] > 5:
-                                print("Your score is too high. Don't try to trick me.")
-                                del real_score[:]
-                                score_checker()
-                            elif real_score[0] < 0 or real_score[1] < 0:
-                                print("Your score is too low. Do you even know what positive numbers are?")
+                            if real_score[0] != 5 and real_score[1] != 5:
+                                print("Your score is too high or low. Don't try to trick me.")
                                 del real_score[:]
                                 score_checker()
                             else:
                                 #always team 1 wins, never team2
                                 if real_score[0] > real_score[1]:
                                     print("Game over. Good job to "+game[0]+" and " + game[1] + " for winning. Thanks for playing....\n")
-                                    winners.append(game[0],game[1])
-                                    losers.append(game[2],game[3])
+                                    winners.append(game[0])
+                                    winners.append(game[1])
+                                    losers.append(game[2])
+                                    losers.append(game[3])
+                                    update_elo()
                                 elif real_score[0] < real_score[1]:
                                     print("Game over. Good job to "+game[2]+" and " + game[3] + " for winning. Thanks for playing....\n")
-                                    winners.append(game[2],game[3])
-                                    losers.append(game[0],game[1])
+                                    winners.append(game[2])
+                                    winners.append(game[3])
+                                    losers.append(game[0])
+                                    losers.append(game[1])
+                                    update_elo()
                                 time.sleep(1)
                                 del real_score[:]
                                 del game[:]
@@ -106,23 +120,37 @@ while True:
                                 time.sleep(0.5)
                                 run()
                         elif int(gametype_input) == 3:
-                            print("gametype_received")
-                            if real_score[0] > 3 or real_score[1] > 3:
+                            if real_score[0] != 3 and real_score[1] != 3:
                                 print("Your score is too high. Don't try to trick me.")
                                 del real_score[:]
                                 score_checker()
-                            elif real_score[0] < 0 or real_score[1] < 0:
-                                print("Your score is too low. Do you even know what positive numbers are?")
-                                del real_score[:]
-                                score_checker()
                             else: 
-                                del real_score[:]
-                                print("Game over. Good job to "+game[0]+" and " + game[1] + " for winning. Thanks for playing....\n")
+                                
+                                if real_score[0] > real_score[1]:
+                                    print("Game over. Good job to "+game[0]+" and " + game[1] + " for winning. Thanks for playing....\n")
+                                    print("Actually, I couldn't care less if you played.\n")
+                                    winners.append(game[0])
+                                    winners.append(game[1])
+                                    losers.append(game[2])
+                                    losers.append(game[3])
+                                    update_elo()
+                                elif real_score[0] < real_score[1]:
+                                    print("Game over. Good job to "+game[2]+" and " + game[3] + " for winning. Thanks for playing....\n")
+                                    print("Actually, I couldn't care less if you played.\n")
+                                    winners.append(game[2])
+                                    winners.append(game[3])
+                                    losers.append(game[0])
+                                    losers.append(game[1])
+                                    update_elo()
                                 time.sleep(1)
+                                del real_score[:]
                                 del game[:]
-                                print("Actually, I couldn't care less if you played.\n")
+                                
+                                
                                 time.sleep(0.5)
-                                run()                           
+                                run()
+                        else:
+                            print("Gametype not received. How hard is it to choose between 3 and 5?")
                     if ingame_input == 'c':
                         del game[:]
                         print("\nGame canceled. Why the F**K did you set it up in the first place?\n")
